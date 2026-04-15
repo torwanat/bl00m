bdccb419185ce605981ce7091233c491438e6bfa
torwanat <nawrotm37@gmail.com>
2026-04-15 12:29:21 +0300

    add pico code

---
+++ bl00m/bl00m.c
@@ -0,0 +1,16 @@
+#include <hardware/gpio.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+
+int main()
+{
+    stdio_init_all();
+
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
